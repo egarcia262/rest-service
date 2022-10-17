3a9315571751b02269128bb2df8d938701e31d3d
Edu <garcia_eduher@gva.es>
2022-10-17 17:03:01 +0200

    Setting parameters for DB

--- src/main/java/com/vlimes/restservice/model/BaseEntity.java
+++ src/main/java/com/vlimes/restservice/model/BaseEntity.java
@@ -2,16 +2,20 @@ package com.vlimes.restservice.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.SequenceGenerator;
 
 @MappedSuperclass
 public class BaseEntity implements Serializable{
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
+	@SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
 	private Long id;
 	
 	public Long getId() {
